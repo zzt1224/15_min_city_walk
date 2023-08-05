cba6450f9a894cec2927b140d3853b992a8f1733
Joe Zhou <joe.zhou@stey.com>
2023-08-05 17:52:47 -0400

    finished web map

--- main.py
+++ main.py
@@ -3,36 +3,56 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 import osmnx as ox
+from shapely.geometry import Point, Polygon
+
+place = '360 Huntington Ave, Boston, MA, United States'
+# (string {"all_private", "all", "bike", "drive", "drive_service", "walk"})
+network_type = 'walk'
+trip_time = 15  # in minutes
+travel_speed = 4.5  # walking speed in km/hour
+meters_per_minute = travel_speed * 1000 / 60
+cols = ["name", "amenity"]
+point_of_interest={"Building": True}
+# https://osmnx.readthedocs.io/en/stable/user-reference.html#module-osmnx.features
+point_of_interest = {"amenity":True, "landuse":["retail","commercial"], "highway":"bus_stop"}
 
 
 def main():
-    place = 'Chapinero,Bogotá,Colombia'
-    network_type = 'walk'
-    trip_times = [5, 10, 15, 25]  # in minutes
-    travel_speed = 4.5  # walking speed in km/hour
+    ## Get graph from OSM
     Graph = ox.graph_from_address(place, network_type=network_type)
 
-    center_node = ox.nearest_nodes(Graph, 4.6519456, -74.0553)
+    center_node = ox.nearest_nodes(Graph, 0, 0)
 
-    meters_per_minute = travel_speed * 1000 / 60
     for u, v, k, data in Graph.edges(data=True, keys=True):
         data['time'] = data['length'] / meters_per_minute
 
-        # get one color for each isochrone
-    iso_colors = ox.plot.get_colors(
-        n=len(trip_times), cmap='plasma', start=0, return_hex=True)
-    node_colors = {}
-    for trip_time, color in zip(sorted(trip_times, reverse=True), iso_colors):
-        subgraph = nx.ego_graph(
-            Graph, center_node, radius=trip_time,  distance='time')
-        for node in subgraph.nodes():
-            node_colors[node] = color
-    nc = [node_colors[node]
-          if node in node_colors else 'none' for node in Graph.nodes()]
-    ns = [15 if node in node_colors else 0 for node in Graph.nodes()]
-    # plot graph
-    fig, ax = ox.plot_graph(Graph, node_color=nc, node_size=ns, node_alpha=0.8,
-                            node_zorder=2, bgcolor='k', edge_linewidth=0.2, edge_color='#999999')
+    ## Get sub graph from the Graph with the radius of trip time    
+    subgraph = nx.ego_graph(Graph, center_node, radius=trip_time,  distance='time')
+    
+    ## Get features from the sub graph
+    features = ox.features_from_address(place, tags=point_of_interest, dist=trip_time * meters_per_minute * 2)
+
+    def get_distance_from_nearest_node(geometry):
+        distance = float('inf')
+        if(type(geometry) == Polygon):
+            nearest = ox.nearest_nodes(subgraph, geometry.centroid.x, geometry.centroid.y)
+            distance = ox.distance.euclidean_dist_vec(Graph.nodes[nearest]['y'], Graph.nodes[nearest]['x'], geometry.centroid.y, geometry.centroid.x)
+        return distance
+    
+
+    # filter features that are too far from the sub graph
+    filtered_features = features[features['geometry'].apply(get_distance_from_nearest_node) < 0.0005]
+
+    # https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.explore.html#geopandas-geodataframe-explore
+    m = filtered_features.explore(tooltip=cols, cmap = "inferno_r")
+
+    edges = ox.graph_to_gdfs(subgraph, nodes=False)
+
+    edges_map = edges.explore(m=m, column = "length", popup = True, tiles = "CartoDB positron", cmap = "inferno_r")
+                                        
+    output_file_path = "index.html"
+
+    edges_map.save(output_file_path)
 
 
 if __name__ == "__main__":
